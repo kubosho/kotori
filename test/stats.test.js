53432d6df840644537414db63ef4288f390ded8d
kubosho <ta2@o2p.jp>
2015-10-16 23:29:09 +0900

    Refactoring of stats.js
    - Rewrite to class base
    - Delete unnecessary package import
    - Add public API (Stats#parseCSS, Stats#writeStats)
    - Add ESDoc

--- test/stats.test.js
+++ test/stats.test.js
@@ -2,22 +2,56 @@
 
 import fs from "fs";
 import File from "vinyl";
-import stats from "../src/stats";
+import Stats from "../src/stats";
 
-/** @test {stats} */
-describe("stats", () => {
-  it("output stats data (JSON format) in stats/", () => {
+/** @test {Stats} */
+describe("Stats", () => {
+  /** @test {Stats#parseCSS} */
+  describe("Stats#parseCSS", () => {
     const statsConfig = {
       stats: {
-        outputFormat: "json",
+        outputFormat: "json"
+      }
+    };
+    const stats = new Stats(`${process.cwd()}/test/cases/main.css`, statsConfig.stats);
+
+    it("outputs the stats data (JSON format) after CSS file parse", () => {
+      return stats.parseCSS()
+        .then((result) => {
+          assert.notStrictEqual(result.published, void 0);
+        })
+        .catch((err) => {
+          assert.strictEqual(err, null);
+        });
+    });
+  });
+
+  /** @test {Stats#writeStats} */
+  describe("Stats#writeStats", () => {
+    const statsConfig = {
+      stats: {
+        outputFormat: "html",
         outputDir   : "stats"
       }
     };
+    const stats = new Stats(`${process.cwd()}/test/cases/main.css`, statsConfig.stats);
+    let statsTestData = null;
 
     before((done) => {
       fs.readFile(`${process.cwd()}/test/cases/stats-test-data.json`, (err, data) => {
         statsTestData = data;
         done();
       });
+    });
+
+    it("output stats data (HTML format) in stats/", () => {
+      return stats.writeStats(statsTestData)
+        .then((result) => {
+          assert.notStrictEqual(result, null);
+        })
+        .catch((err) => {
+          assert.strictEqual(err, null);
+        });
+    });
   });
 });
